be2789cc94ae2128465d4e4a07a62a38bda62553
abystry <arek.bystrowicz@gmail.com>
2018-06-27 17:41:42 +0200

    Add class attributes

--- server/src/main/java/com/cafetamine/surveys/category/Category.java
+++ server/src/main/java/com/cafetamine/surveys/category/Category.java
@@ -1,4 +1,24 @@
 package com.cafetamine.surveys.category;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import java.util.Date;
+
+
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String tag;
+    @NotNull
+    private Date creationDate;
+
 }
