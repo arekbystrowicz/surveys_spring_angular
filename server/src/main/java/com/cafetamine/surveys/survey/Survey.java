61a593c49d26f012c5319fbd15e606b00c904aab
abystry <arek.bystrowicz@gmail.com>
2018-06-27 17:22:01 +0200

    Add class attributes and orm annotations

--- server/src/main/java/com/cafetamine/surveys/survey/Survey.java
+++ server/src/main/java/com/cafetamine/surveys/survey/Survey.java
@@ -1,5 +1,32 @@
 package com.cafetamine.surveys.survey;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
+import java.util.Date;
+
+
+@Entity
 public class Survey {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String title;
+    @NotEmpty
+    private String description;
+    @NotNull
+    private Date creationDate;
+    private Date lastUpdateDate;
+    private Date publishDate;
+    @NotNull
+    private Boolean isPublished;
+    @NotNull
+    private Boolean isAccessible;
+
 }
