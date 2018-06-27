b41cf386f2dc8d195d8000ae274f3704f7628f41
abystry <arek.bystrowicz@gmail.com>
2018-06-27 17:05:36 +0200

    Generate spring boot project (web,jpa,postgresql)

---
+++ server/src/main/java/com/cafetamine/surveys/SurveysApplication.java
@@ -0,0 +1,12 @@
+package com.cafetamine.surveys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveysApplication.class, args);
+	}
+}
