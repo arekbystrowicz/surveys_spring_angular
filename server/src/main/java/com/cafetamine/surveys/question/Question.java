e410c48e8234f61345e41495c6f43ac4ca6e39a4
abystry <arek.bystrowicz@gmail.com>
2018-06-27 17:26:42 +0200

    Add class attributes

--- server/src/main/java/com/cafetamine/surveys/question/Question.java
+++ server/src/main/java/com/cafetamine/surveys/question/Question.java
@@ -1,5 +1,31 @@
 package com.cafetamine.surveys.question;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
+import java.util.Date;
+
+import com.cafetamine.surveys.survey.Survey;
+
+
+@Entity
 public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String title;
+    @NotEmpty
+    private String description;
+    @NotNull
+    private Date creationDate;
+    private Date lastUpdateDate;
+    @NotNull
+    private Boolean isAccessible;
+    @NotNull
+    @OneToOne
+    private Survey survey;
+
 }
