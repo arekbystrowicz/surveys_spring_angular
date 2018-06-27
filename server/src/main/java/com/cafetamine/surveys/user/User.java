3e628f5879ea5d43c9de780b3376d11d35c5f1a0
abystry <arek.bystrowicz@gmail.com>
2018-06-27 17:57:57 +0200

    Add class attributes

--- server/src/main/java/com/cafetamine/surveys/user/User.java
+++ server/src/main/java/com/cafetamine/surveys/user/User.java
@@ -1,4 +1,33 @@
 package com.cafetamine.surveys.user;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import java.util.Date;
+
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String login;
+    @NotEmpty
+    private String email;
+    @NotEmpty
+    private String password;
+    @NotEmpty
+    private  String name;
+    @NotEmpty
+    private String surname;
+    @NotNull
+    private Date joinDate;
+    private Date lastUpdateDate;
+    @NotNull
+    private Boolean isActive;
+
 }
