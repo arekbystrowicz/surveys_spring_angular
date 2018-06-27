8e7d1a3a0832f34e6e745c9639bb5f78ae46f473
abystry <arek.bystrowicz@gmail.com>
2018-06-27 18:05:13 +0200

    Add class attributes

--- server/src/main/java/com/cafetamine/surveys/solution/Solution.java
+++ server/src/main/java/com/cafetamine/surveys/solution/Solution.java
@@ -1,5 +1,25 @@
 package com.cafetamine.surveys.solution;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.NotNull;
 
+import com.cafetamine.surveys.answer.Answer;
+import com.cafetamine.surveys.user.User;
+
+
+
+@Entity
 public class Solution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private User author;
+    @NotNull
+    private Answer answer;
+
 }
