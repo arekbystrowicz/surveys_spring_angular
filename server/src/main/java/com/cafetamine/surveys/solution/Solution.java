574a9ffb306624f897b74d06c63119305db8be03
abystry <arek.bystrowicz@gmail.com>
2018-06-27 18:09:05 +0200

    Add getters and setters

--- server/src/main/java/com/cafetamine/surveys/solution/Solution.java
+++ server/src/main/java/com/cafetamine/surveys/solution/Solution.java
@@ -26,4 +26,44 @@ public class Solution {
     private Date creationDate;
     private Date lastUpdateDate;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
 }
