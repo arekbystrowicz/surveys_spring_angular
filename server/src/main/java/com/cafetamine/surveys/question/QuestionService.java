cd15d9c9d7385bced622486e04e8d28bdcb26bdf
abystry <arek.bystrowicz@gmail.com>
2018-07-03 13:12:22 +0200

    Add method getById

--- server/src/main/java/com/cafetamine/surveys/question/QuestionService.java
+++ server/src/main/java/com/cafetamine/surveys/question/QuestionService.java
@@ -2,6 +2,8 @@ package com.cafetamine.surveys.question;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class QuestionService {
@@ -10,6 +12,15 @@ public class QuestionService {
 
     public QuestionService(QuestionRepository questionRepository) {
         this.questionRepository = questionRepository;
+    }
+
+    public Question getById(Long id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (!question.isPresent()) {
+            // TODO exception
+            throw new RuntimeException("404 question not found");
+        }
+        return question.get();
     }
 
 }
