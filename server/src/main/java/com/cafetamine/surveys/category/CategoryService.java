867f44056353945542a81365958ea8d4312c97b2
abystry <arek.bystrowicz@gmail.com>
2018-07-03 14:06:41 +0200

    Add getByTag method

--- server/src/main/java/com/cafetamine/surveys/category/CategoryService.java
+++ server/src/main/java/com/cafetamine/surveys/category/CategoryService.java
@@ -2,6 +2,8 @@ package com.cafetamine.surveys.category;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class CategoryService {
@@ -10,6 +12,15 @@ public class CategoryService {
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    public Category getByTag(String tag) {
+        Optional<Category> category = this.categoryRepository.findByTag(tag);
+        if (!category.isPresent()) {
+            // TODO exception
+            throw new RuntimeException("404 category not found");
+        }
+        return category.get();
     }
 
 }
