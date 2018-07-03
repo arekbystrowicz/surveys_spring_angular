f8509a482e36efa5771f490f793b136fd785ef00
abystry <arek.bystrowicz@gmail.com>
2018-07-03 10:15:38 +0200

    Add method declaration findByIdAndIsActive

--- server/src/main/java/com/cafetamine/surveys/user/UserRepository.java
+++ server/src/main/java/com/cafetamine/surveys/user/UserRepository.java
@@ -3,7 +3,13 @@ package com.cafetamine.surveys.user;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+
+    Optional<User> findByIdAndIsActive(Long id, Boolean isActive);
+
 }
