d731257eb1f851a668d02f8333f4139e2705e341
abystry <arek.bystrowicz@gmail.com>
2018-07-03 11:34:51 +0200

    Add method getById

--- server/src/main/java/com/cafetamine/surveys/user/UserService.java
+++ server/src/main/java/com/cafetamine/surveys/user/UserService.java
@@ -2,6 +2,8 @@ package com.cafetamine.surveys.user;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class UserService {
@@ -11,5 +13,14 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    
+
+    public User getById(Long id) {
+        Optional<User> user = this.userRepository.findByIdAndIsActive(id, true);
+        if (!user.isPresent()) {
+            // TODO exception
+            throw new RuntimeException("404 user not found");
+        }
+        return user.get();
+    }
+
 }
