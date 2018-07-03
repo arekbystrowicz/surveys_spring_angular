f75e49cdc9478ff27cd7869b5c4863f3b0f5d66a
abystry <arek.bystrowicz@gmail.com>
2018-07-03 11:25:27 +0200

    Add method declaration findByAnswer

--- server/src/main/java/com/cafetamine/surveys/solution/SolutionRepository.java
+++ server/src/main/java/com/cafetamine/surveys/solution/SolutionRepository.java
@@ -3,7 +3,12 @@ package com.cafetamine.surveys.solution;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
+import com.cafetamine.surveys.answer.Answer;
+
 
 @Repository
 public interface SolutionRepository extends CrudRepository<Solution, Long> {
+
+    Iterable<Solution> findAllByAnswer(Answer answer);
+
 }
