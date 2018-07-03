62d168c7391f56d9dc2ca294c8db6eb9d88a0bab
abystry <arek.bystrowicz@gmail.com>
2018-07-03 14:04:55 +0200

    Add CategoryRepository and constructor

--- server/src/main/java/com/cafetamine/surveys/category/CategoryService.java
+++ server/src/main/java/com/cafetamine/surveys/category/CategoryService.java
@@ -5,4 +5,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
 }
