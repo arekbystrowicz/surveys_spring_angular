87bafae01c8578403a594034d0216f4d3b605c0c
abystry <arek.bystrowicz@gmail.com>
2018-07-03 11:16:49 +0200

    Add method declaration findBySurevy

--- server/src/main/java/com/cafetamine/surveys/question/QuestionRepository.java
+++ server/src/main/java/com/cafetamine/surveys/question/QuestionRepository.java
@@ -3,7 +3,11 @@ package com.cafetamine.surveys.question;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
+import com.cafetamine.surveys.survey.Survey;
+
 
 @Repository
 public interface QuestionRepository extends CrudRepository<Question, Long> {
+
+    Iterable<Question> findAllBySurvey(Survey survey);
 }
