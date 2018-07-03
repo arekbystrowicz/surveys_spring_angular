45d73448f4dc344858d30c9e78c981c94dc35ba5
abystry <arek.bystrowicz@gmail.com>
2018-07-03 11:50:38 +0200

    Add spring bean annotation, class field SurveyRepo and its dependency injection in constructor

--- server/src/main/java/com/cafetamine/surveys/survey/SurveyService.java
+++ server/src/main/java/com/cafetamine/surveys/survey/SurveyService.java
@@ -1,4 +1,15 @@
 package com.cafetamine.surveys.survey;
 
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class SurveyService {
+
+    private SurveyRepository surveyRepository;
+
+    public SurveyService(SurveyRepository surveyRepository) {
+        this.surveyRepository = surveyRepository;
+    }
+
 }
