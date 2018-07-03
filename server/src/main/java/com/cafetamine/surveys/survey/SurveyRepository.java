bdda417a2ac6bc311b042c0b44a4a0edb2ea317b
abystry <arek.bystrowicz@gmail.com>
2018-07-03 10:28:41 +0200

    Adjust code consistency -> delete is with booleans in method names

--- server/src/main/java/com/cafetamine/surveys/survey/SurveyRepository.java
+++ server/src/main/java/com/cafetamine/surveys/survey/SurveyRepository.java
@@ -11,9 +11,9 @@ import com.cafetamine.surveys.user.User;
 @Repository
 public interface SurveyRepository extends CrudRepository<Survey, Long> {
 
-    Optional<Survey> findByIdAndIsAccessibleAndIsPublished(Long id, Boolean isAccessible, Boolean isPublished);
+    Optional<Survey> findByIdAndAccessibleAndPublished(Long id, Boolean isAccessible, Boolean isPublished);
 
-    Iterable<Survey> findAllByIsAccessibleAndIsPublished(Boolean isAccessible, Boolean isPublished);
+    Iterable<Survey> findAllByAccessibleAndPublished(Boolean isAccessible, Boolean isPublished);
 
     Iterable<Survey> findAllByAuthorAndAccessibleAndPublished(User author, Boolean isAccessible, Boolean isPublished);
 
