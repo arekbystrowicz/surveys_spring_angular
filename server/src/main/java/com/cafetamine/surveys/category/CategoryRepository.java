cb186089730e15871295ced10129adaacf5cb834
abystry <arek.bystrowicz@gmail.com>
2018-07-03 10:40:15 +0200

    Add spring bean annotation and extension of crud repo

--- server/src/main/java/com/cafetamine/surveys/category/CategoryRepository.java
+++ server/src/main/java/com/cafetamine/surveys/category/CategoryRepository.java
@@ -1,4 +1,11 @@
 package com.cafetamine.surveys.category;
 
-public interface CategoryRepository {
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Long> {
 }
