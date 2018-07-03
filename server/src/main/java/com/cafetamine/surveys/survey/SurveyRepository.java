6a1a13ddb24c9f3dfae1c5c0025628761f1042cb
abystry <arek.bystrowicz@gmail.com>
2018-07-03 10:30:57 +0200

    Add method declaration findAllByCategoriesContainingAndAccessibleAndPublished

--- server/src/main/java/com/cafetamine/surveys/survey/SurveyRepository.java
+++ server/src/main/java/com/cafetamine/surveys/survey/SurveyRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 import com.cafetamine.surveys.user.User;
+import com.cafetamine.surveys.category.Category;
 
 
 @Repository
@@ -16,5 +17,7 @@ public interface SurveyRepository extends CrudRepository<Survey, Long> {
     Iterable<Survey> findAllByAccessibleAndPublished(Boolean isAccessible, Boolean isPublished);
 
     Iterable<Survey> findAllByAuthorAndAccessibleAndPublished(User author, Boolean isAccessible, Boolean isPublished);
+
+    Iterable<Survey> findAllByCategoriesContainingAndAccessibleAndPublished(Category category, Boolean isAccessible, Boolean isPublished);
 
 }
