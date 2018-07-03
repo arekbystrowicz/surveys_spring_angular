900e9c1402e4ad9223e32af46a449d361655ae4f
abystry <arek.bystrowicz@gmail.com>
2018-07-03 12:34:25 +0200

    Add private method getByIdAccessibleAndPublished

--- server/src/main/java/com/cafetamine/surveys/survey/SurveyService.java
+++ server/src/main/java/com/cafetamine/surveys/survey/SurveyService.java
@@ -2,6 +2,8 @@ package com.cafetamine.surveys.survey;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class SurveyService {
@@ -10,6 +12,15 @@ public class SurveyService {
 
     public SurveyService(SurveyRepository surveyRepository) {
         this.surveyRepository = surveyRepository;
+    }
+
+    private Survey getByIdAndAccessibleAndPublished(Long id, Boolean isAccessible, Boolean isPublished) {
+        Optional<Survey> survey = this.surveyRepository.findByIdAndAccessibleAndPublished(id, isAccessible, isPublished);
+        if (!survey.isPresent()) {
+            // TODO exception
+            throw new RuntimeException("404 survey not found");
+        }
+        return survey.get();
     }
 
 }
