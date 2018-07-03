dfb03f59bf1de31eb39e93f95c486ccaadb163ee
abystry <arek.bystrowicz@gmail.com>
2018-07-03 10:19:54 +0200

    Add spring bean annotation and extension of crude repo

--- server/src/main/java/com/cafetamine/surveys/survey/SurveyRepository.java
+++ server/src/main/java/com/cafetamine/surveys/survey/SurveyRepository.java
@@ -1,4 +1,9 @@
 package com.cafetamine.surveys.survey;
 
-public interface SurveyRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface SurveyRepository extends CrudRepository<Survey, Long> {
 }
