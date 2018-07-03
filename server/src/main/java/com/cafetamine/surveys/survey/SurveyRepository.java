7b49fd4d241268a3009bc9876d535182a4c26dbb
abystry <arek.bystrowicz@gmail.com>
2018-07-03 10:21:29 +0200

    Add method declaration findByIdAndIsAccessible

--- server/src/main/java/com/cafetamine/surveys/survey/SurveyRepository.java
+++ server/src/main/java/com/cafetamine/surveys/survey/SurveyRepository.java
@@ -3,7 +3,12 @@ package com.cafetamine.surveys.survey;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface SurveyRepository extends CrudRepository<Survey, Long> {
+
+    Optional<Survey> findByIdAndIsAccessible(Long id, Boolean isAccessible);
+
 }
