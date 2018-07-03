81b5f2e2fb0e15fbb8ffbbbfe65712022f0a94e1
abystry <arek.bystrowicz@gmail.com>
2018-07-03 11:31:39 +0200

    Add spring bean annotation, class attribiute userRepository and constructor

--- server/src/main/java/com/cafetamine/surveys/user/UserService.java
+++ server/src/main/java/com/cafetamine/surveys/user/UserService.java
@@ -1,4 +1,15 @@
 package com.cafetamine.surveys.user;
 
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
 }
