e51f527d02a593c508dd507f8b24748d4bb0e9ca
abystry <arek.bystrowicz@gmail.com>
2018-07-03 11:26:30 +0200

    Add method declaration findAllByAnswerAndAuthor

--- server/src/main/java/com/cafetamine/surveys/solution/SolutionRepository.java
+++ server/src/main/java/com/cafetamine/surveys/solution/SolutionRepository.java
@@ -3,6 +3,7 @@ package com.cafetamine.surveys.solution;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
+import com.cafetamine.surveys.user.User;
 import com.cafetamine.surveys.answer.Answer;
 
 
@@ -10,5 +11,7 @@ import com.cafetamine.surveys.answer.Answer;
 public interface SolutionRepository extends CrudRepository<Solution, Long> {
 
     Iterable<Solution> findAllByAnswer(Answer answer);
+
+    Iterable<Solution> findAllByAnswerAndAuthor(Answer answer, User author);
 
 }
