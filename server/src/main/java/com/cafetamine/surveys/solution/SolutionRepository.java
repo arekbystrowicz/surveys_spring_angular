76abb1ad03b77282b0b0bfb52cc5911a8490f7c6
abystry <arek.bystrowicz@gmail.com>
2018-07-03 11:24:58 +0200

    Add spring bean annotation and extension of crud repo

--- server/src/main/java/com/cafetamine/surveys/solution/SolutionRepository.java
+++ server/src/main/java/com/cafetamine/surveys/solution/SolutionRepository.java
@@ -1,4 +1,9 @@
 package com.cafetamine.surveys.solution;
 
-public interface SolutionRepository {
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+
+@Repository
+public interface SolutionRepository extends CrudRepository<Solution, Long> {
 }
