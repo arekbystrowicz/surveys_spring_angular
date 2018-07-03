e22f51fe632aa8789b17cf619330de8a27d4d02e
abystry <arek.bystrowicz@gmail.com>
2018-07-03 13:21:05 +0200

    Add optionals

--- server/src/main/java/com/cafetamine/surveys/question/QuestionRepository.java
+++ server/src/main/java/com/cafetamine/surveys/question/QuestionRepository.java
@@ -3,13 +3,15 @@ package com.cafetamine.surveys.question;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 import com.cafetamine.surveys.survey.Survey;
 
 
 @Repository
 public interface QuestionRepository extends CrudRepository<Question, Long> {
 
-    Question findByIdAndAccessible(Long id, Boolean isAccessible);
+    Optional<Question> findByIdAndAccessible(Long id, Boolean isAccessible);
 
     Iterable<Question> findAllBySurveyAndAccessible(Survey survey, Boolean isAccessible);
 
