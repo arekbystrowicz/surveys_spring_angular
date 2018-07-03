8e2faf7af27aaa22edf2c6363366a4a9b3403ef3
abystry <arek.bystrowicz@gmail.com>
2018-07-03 12:55:17 +0200

    Add questionService as class field, and its dependency injection in constructor

--- server/src/main/java/com/cafetamine/surveys/question/QuestionService.java
+++ server/src/main/java/com/cafetamine/surveys/question/QuestionService.java
@@ -1,4 +1,16 @@
 package com.cafetamine.surveys.question;
 
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class QuestionService {
+
+    private QuestionRepository questionRepository;
+
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
 }
+
