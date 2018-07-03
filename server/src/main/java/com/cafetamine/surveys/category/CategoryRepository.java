ca5d6ca6e17ac21d6b94262503caf108514a2fbf
abystry <arek.bystrowicz@gmail.com>
2018-07-03 10:43:43 +0200

    Add method declaration findByAuthor

--- server/src/main/java/com/cafetamine/surveys/category/CategoryRepository.java
+++ server/src/main/java/com/cafetamine/surveys/category/CategoryRepository.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import com.cafetamine.surveys.user.User;
 
 
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
     Optional<Category> findByTag(String tag);
+
+    Iterable<Category> findByAuthor(User author);
 
 }
