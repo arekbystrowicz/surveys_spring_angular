831e56cadee64fd2360458b1c56869bd23ebfdd4
abystry <arek.bystrowicz@gmail.com>
2018-07-03 14:19:32 +0200

    Add solutionRepository as class field and constructor with its dependency injection

--- server/src/main/java/com/cafetamine/surveys/solution/SolutionService.java
+++ server/src/main/java/com/cafetamine/surveys/solution/SolutionService.java
@@ -5,4 +5,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SolutionService {
+
+    private SolutionRepository solutionRepository;
+
+    public SolutionService(SolutionRepository solutionRepository) {
+        this.solutionRepository = solutionRepository;
+    }
+
 }
