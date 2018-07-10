e4dda15d7b0569ab12a3f5aa917328529fb5b62c
abystry <arek.bystrowicz@gmail.com>
2018-07-10 17:54:00 +0200

    Add spring bean annotation and request mapping

--- server/src/main/java/com/cafetamine/surveys/question/QuestionController.java
+++ server/src/main/java/com/cafetamine/surveys/question/QuestionController.java
@@ -1,4 +1,10 @@
 package com.cafetamine.surveys.question;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("question")
 public class QuestionController {
 }
