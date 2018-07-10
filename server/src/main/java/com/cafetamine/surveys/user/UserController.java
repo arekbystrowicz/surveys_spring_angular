da67135746430eebe5ee38abd7437d51f47b5a7b
abystry <arek.bystrowicz@gmail.com>
2018-07-10 15:17:09 +0200

    Add userRepository ass class field, Add constructor with its dependency injection

--- server/src/main/java/com/cafetamine/surveys/user/UserController.java
+++ server/src/main/java/com/cafetamine/surveys/user/UserController.java
@@ -7,4 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("users")
 public class UserController {
+
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
 }
