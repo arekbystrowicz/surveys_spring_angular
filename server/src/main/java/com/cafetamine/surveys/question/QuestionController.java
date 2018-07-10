ba43cf045bfa3605a8a4732701af23bfae79aa86
abystry <arek.bystrowicz@gmail.com>
2018-07-10 17:59:26 +0200

    Add SurevyService as class field, Add its dependency injection in constructor

--- server/src/main/java/com/cafetamine/surveys/question/QuestionController.java
+++ server/src/main/java/com/cafetamine/surveys/question/QuestionController.java
@@ -3,15 +3,19 @@ package com.cafetamine.surveys.question;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cafetamine.surveys.survey.SurveyService;
+
 
 @RestController
 @RequestMapping("question")
 public class QuestionController {
 
     private QuestionService questionService;
+    private SurveyService surveyService;
 
-    public QuestionController(QuestionService questionService) {
+    public QuestionController(QuestionService questionService, SurveyService surveyService) {
         this.questionService = questionService;
+        this.surveyService = surveyService;
     }
 
 }
