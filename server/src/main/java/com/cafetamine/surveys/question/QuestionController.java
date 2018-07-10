52c4b97a01301bd8c51f1c024d58dec252f2a3a8
abystry <arek.bystrowicz@gmail.com>
2018-07-10 17:54:44 +0200

    Add questionService as class field and constructor with its dependency injection

--- server/src/main/java/com/cafetamine/surveys/question/QuestionController.java
+++ server/src/main/java/com/cafetamine/surveys/question/QuestionController.java
@@ -7,4 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("question")
 public class QuestionController {
+
+    private QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
 }
