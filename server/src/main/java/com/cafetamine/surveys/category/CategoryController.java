8b1e61721705e2dfcfeb97afc3e8a5b8b0b5d5ae
abystry <arek.bystrowicz@gmail.com>
2018-07-10 16:53:41 +0200

    Add request mapping and spring bean annotation

--- server/src/main/java/com/cafetamine/surveys/category/CategoryController.java
+++ server/src/main/java/com/cafetamine/surveys/category/CategoryController.java
@@ -1,4 +1,10 @@
 package com.cafetamine.surveys.category;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/categories")
 public class CategoryController {
 }
