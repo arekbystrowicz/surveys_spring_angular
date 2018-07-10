fb4eeef11fd381c3c4cc9fa50d5b8a09d9589dff
abystry <arek.bystrowicz@gmail.com>
2018-07-10 18:03:31 +0200

    Add method getBySurvey

--- server/src/main/java/com/cafetamine/surveys/question/QuestionController.java
+++ server/src/main/java/com/cafetamine/surveys/question/QuestionController.java
@@ -1,7 +1,9 @@
 package com.cafetamine.surveys.question;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cafetamine.surveys.survey.SurveyService;
 
@@ -16,6 +18,11 @@ public class QuestionController {
     public QuestionController(QuestionService questionService, SurveyService surveyService) {
         this.questionService = questionService;
         this.surveyService = surveyService;
+    }
+
+    @GetMapping(params = {"survey_id={id}", "action=solve"})
+    public Iterable<Question> getBySurvey(@RequestParam("survey_id") Long id) {
+        return this.questionService.getAllBySurvey(this.surveyService.getPublishedById(id));
     }
 
 }
