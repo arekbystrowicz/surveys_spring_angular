2ed95d77d5c9ce478f709061caacd41854e9c250
abystry <arek.bystrowicz@gmail.com>
2018-07-10 17:02:03 +0200

    Add userService as class field, Add its dependency injection in constructor

--- server/src/main/java/com/cafetamine/surveys/category/CategoryController.java
+++ server/src/main/java/com/cafetamine/surveys/category/CategoryController.java
@@ -1,9 +1,10 @@
 package com.cafetamine.surveys.category;
 
-import org.springframework.web.bind.annotation.RequestParam;
+import com.cafetamine.surveys.user.UserService;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CategoryController {
 
     private CategoryService categoryService;
+    private UserService userService;
 
-    public CategoryController(CategoryService categoryService) {
+    public CategoryController(CategoryService categoryService, UserService userService) {
         this.categoryService = categoryService;
+        this.userService = userService;
     }
 
     @GetMapping(params = "action=show_all")
