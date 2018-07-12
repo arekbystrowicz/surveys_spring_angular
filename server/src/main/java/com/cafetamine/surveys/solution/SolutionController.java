c3dc48f46975453307d24ca0be55ca35f78f4ddf
abystry <arek.bystrowicz@gmail.com>
2018-07-12 11:33:14 +0200

    Add SolutionService as class field, Add constructor

--- server/src/main/java/com/cafetamine/surveys/solution/SolutionController.java
+++ server/src/main/java/com/cafetamine/surveys/solution/SolutionController.java
@@ -7,4 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("solutions")
 public class SolutionController {
+
+    private SolutionService solutionService;
+
+    public SolutionController(SolutionService solutionService) {
+        this.solutionService = solutionService;
+    }
+
 }
