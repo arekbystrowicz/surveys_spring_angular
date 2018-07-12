d6af9e261a0c2f5075f533a85e081119f60bda18
abystry <arek.bystrowicz@gmail.com>
2018-07-12 10:59:01 +0200

    Add spring bean declaration and request mapping for /answers path

--- server/src/main/java/com/cafetamine/surveys/answer/AnswerController.java
+++ server/src/main/java/com/cafetamine/surveys/answer/AnswerController.java
@@ -1,4 +1,10 @@
 package com.cafetamine.surveys.answer;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("answers")
 public class AnswerController {
 }
