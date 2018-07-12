70acfbec6a52a56bfd10372c5533e17644e7c783
abystry <arek.bystrowicz@gmail.com>
2018-07-12 11:48:18 +0200

    Add missing @Entity declaration

--- server/src/main/java/com/cafetamine/surveys/user/User.java
+++ server/src/main/java/com/cafetamine/surveys/user/User.java
@@ -1,5 +1,6 @@
 package com.cafetamine.surveys.user;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -9,6 +10,7 @@ import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 
+@Entity
 public class User {
 
     @Id
