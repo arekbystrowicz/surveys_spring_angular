116ca86aa30fedd8e89a81dbd32f33099af70862
abystry <arek.bystrowicz@gmail.com>
2018-07-12 15:36:14 +0200

    Delete all calls to attribiute isPublished

--- server/src/main/java/com/cafetamine/surveys/survey/SurveyRepository.java
+++ server/src/main/java/com/cafetamine/surveys/survey/SurveyRepository.java
@@ -12,16 +12,14 @@ import com.cafetamine.surveys.category.Category;
 @Repository
 public interface SurveyRepository extends CrudRepository<Survey, Long> {
 
-    Optional<Survey> findByIdAndIsAccessibleAndIsPublished(Long id, Boolean isAccessible, Boolean isPublished);
-
     Optional<Survey> findByIdAndIsAccessible(Long id, Boolean isAccessible);
 
-    Iterable<Survey> findAllByIsAccessibleAndIsPublished(Boolean isAccessible, Boolean isPublished);
+    Iterable<Survey> findAllByIsAccessible(Boolean isPublished);
 
-    Iterable<Survey> findAllByAuthorAndIsAccessibleAndIsPublished(User author, Boolean isAccessible, Boolean isPublished);
+    Iterable<Survey> findAllByAuthorAndIsAccessible(User author, Boolean isAccessible);
 
-    Iterable<Survey> findAllByCategoriesContainingAndIsAccessibleAndIsPublished(Category category, Boolean isAccessible, Boolean isPublished);
+    Iterable<Survey> findAllByCategoriesContainingAndIsAccessible(Category category, Boolean isAccessible);
 
-    Iterable<Survey> findAllByTitleAndIsAccessibleAndIsPublished(String title, Boolean isAccessible, Boolean isPublished);
+    Iterable<Survey> findAllByTitleAndIsAccessible(String title, Boolean isAccessible);
 
 }
