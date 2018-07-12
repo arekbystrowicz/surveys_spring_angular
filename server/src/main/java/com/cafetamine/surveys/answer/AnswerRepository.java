e1bd1a0b485a76eca235edf5e527533cf3b9ba8d
abystry <arek.bystrowicz@gmail.com>
2018-07-12 19:25:13 +0200

    Add findByIdAndIsAccessible method declaation

--- server/src/main/java/com/cafetamine/surveys/answer/AnswerRepository.java
+++ server/src/main/java/com/cafetamine/surveys/answer/AnswerRepository.java
@@ -1,13 +1,17 @@
 package com.cafetamine.surveys.answer;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
 
 import com.cafetamine.surveys.question.Question;
 
 
 @Repository
 public interface AnswerRepository extends CrudRepository<Answer, Long> {
+
+    Optional<Answer> findByIdAndIsAccessible(Long id, Boolean isAccessible);
 
     Iterable<Answer> findAllByQuestionAndIsAccessible(Question question, Boolean isAccessible);
 
