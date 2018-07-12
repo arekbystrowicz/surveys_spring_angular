7b5e62d5a1748822890b322061e7a27a54aaab5f
abystry <arek.bystrowicz@gmail.com>
2018-07-12 11:01:09 +0200

    Add getByID method

--- server/src/main/java/com/cafetamine/surveys/answer/AnswerController.java
+++ server/src/main/java/com/cafetamine/surveys/answer/AnswerController.java
@@ -1,5 +1,6 @@
 package com.cafetamine.surveys.answer;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class AnswerController {
 
     public AnswerController(AnswerService answerService) {
         this.answerService = answerService;
+    }
+
+    @RequestMapping("/{id}")
+    public Answer getById(@PathVariable Long id) {
+        return this.answerService.getById(id);
     }
 
 }
