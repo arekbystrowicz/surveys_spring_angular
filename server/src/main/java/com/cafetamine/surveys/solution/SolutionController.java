1dc07cbaec07ed103728d0ce220d6dece1869e6c
abystry <arek.bystrowicz@gmail.com>
2018-07-12 11:34:56 +0200

    Add method getById

--- server/src/main/java/com/cafetamine/surveys/solution/SolutionController.java
+++ server/src/main/java/com/cafetamine/surveys/solution/SolutionController.java
@@ -2,6 +2,8 @@ package com.cafetamine.surveys.solution;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -12,6 +14,11 @@ public class SolutionController {
 
     public SolutionController(SolutionService solutionService) {
         this.solutionService = solutionService;
+    }
+
+    @GetMapping("/{id}")
+    public Solution getById(@PathVariable Long id) {
+        return this.solutionService.getById(id);
     }
 
 }
