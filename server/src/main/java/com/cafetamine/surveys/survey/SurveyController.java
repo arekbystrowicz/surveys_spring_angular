3bd1a19a9b8191d73dd2ab57fa103ff21cb2c1c9
abystry <arek.bystrowicz@gmail.com>
2018-07-12 15:45:53 +0200

    Comment all code before reimplementing class

--- server/src/main/java/com/cafetamine/surveys/survey/SurveyController.java
+++ server/src/main/java/com/cafetamine/surveys/survey/SurveyController.java
@@ -17,40 +17,40 @@ public class SurveyController {
         this.userService = userService;
     }
 
-    @GetMapping(value = "/{id}", params="show=published")
-    public Survey getPublishedById(@PathVariable Long id) {
-        return this.surveyService.getPublishedById(id);
-    }
-
-    @GetMapping(value = "/{id}", params="show=workspace")
-    public Survey getWorkspaceById(@PathVariable Long id) {
-        return this.surveyService.getUnpublishedById(id);
-    }
-
-    @GetMapping(params = "show=published")
-    public Iterable<Survey> getAllPublished() {
-        return this.surveyService.getPublished();
-    }
-
-    @GetMapping(params = "user_id={id}")
-    public Iterable<Survey> getWorkspace(@RequestParam("user_id") Long id) {
-        return this.surveyService.getWorkspace(this.userService.getById(id));
-    }
-
-    @PostMapping()
-    public Survey create(@RequestBody Survey survey) {
-        return this.surveyService.create(survey);
-    }
-
-    @PostMapping(value = "/{id}", params = "action=update")
-    public Survey update(@RequestBody Survey survey) {
-        return this.surveyService.update(survey);
-    }
-
-    @PostMapping(value = "/{id}", params = "action=delete")
-    public Survey delete(@PathVariable Long id) {
-        // TODO change service to allow both published, unpublished
-        return this.surveyService.delete(this.surveyService.getPublishedById(id));
-    }
+//    @GetMapping(value = "/{id}", params="show=published")
+//    public Survey getPublishedById(@PathVariable Long id) {
+//        return this.surveyService.getPublishedById(id);
+//    }
+//
+//    @GetMapping(value = "/{id}", params="show=workspace")
+//    public Survey getWorkspaceById(@PathVariable Long id) {
+//        return this.surveyService.getUnpublishedById(id);
+//    }
+//
+//    @GetMapping(params = "show=published")
+//    public Iterable<Survey> getAllPublished() {
+//        return this.surveyService.getPublished();
+//    }
+//
+//    @GetMapping(params = "user_id={id}")
+//    public Iterable<Survey> getWorkspace(@RequestParam("user_id") Long id) {
+//        return this.surveyService.getWorkspace(this.userService.getById(id));
+//    }
+//
+//    @PostMapping()
+//    public Survey create(@RequestBody Survey survey) {
+//        return this.surveyService.create(survey);
+//    }
+//
+//    @PostMapping(value = "/{id}", params = "action=update")
+//    public Survey update(@RequestBody Survey survey) {
+//        return this.surveyService.update(survey);
+//    }
+//
+//    @PostMapping(value = "/{id}", params = "action=delete")
+//    public Survey delete(@PathVariable Long id) {
+//        // TODO change service to allow both published, unpublished
+//        return this.surveyService.delete(this.surveyService.getPublishedById(id));
+//    }
 
 }
