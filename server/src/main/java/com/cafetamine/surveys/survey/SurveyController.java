7af36bf407c964a6c0a2f69673e0805bcb80f655
abystry <arek.bystrowicz@gmail.com>
2018-07-12 11:13:06 +0200

    Add spring bean declaration and mapping on '/surveys'

--- server/src/main/java/com/cafetamine/surveys/survey/SurveyController.java
+++ server/src/main/java/com/cafetamine/surveys/survey/SurveyController.java
@@ -1,4 +1,10 @@
 package com.cafetamine.surveys.survey;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("surveys")
 public class SurveyController {
 }
