6956d89cc8d19658dfeb63c30b199dfffc723b59
abystry <arek.bystrowicz@gmail.com>
2018-07-12 11:00:00 +0200

    Add class field AnswerService, with dependency injection in constructor

--- server/src/main/java/com/cafetamine/surveys/answer/AnswerController.java
+++ server/src/main/java/com/cafetamine/surveys/answer/AnswerController.java
@@ -7,4 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("answers")
 public class AnswerController {
+
+    private AnswerService answerService;
+
+    public AnswerController(AnswerService answerService) {
+        this.answerService = answerService;
+    }
+
 }
