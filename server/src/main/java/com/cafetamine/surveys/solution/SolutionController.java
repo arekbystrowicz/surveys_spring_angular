6a6101ae10de9593f293e5cd2b096737f5b3290f
abystry <arek.bystrowicz@gmail.com>
2018-07-12 11:32:13 +0200

    Create class SolutionController

---
+++ server/src/main/java/com/cafetamine/surveys/solution/SolutionController.java
@@ -0,0 +1,10 @@
+package com.cafetamine.surveys.solution;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("solutions")
+public class SolutionController {
+}
