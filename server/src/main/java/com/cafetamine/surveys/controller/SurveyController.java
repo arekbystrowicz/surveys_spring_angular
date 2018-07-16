04f53c34b8c54fba8120b3e7f7f35d9d45d52e08
abystry <arek.bystrowicz@gmail.com>
2018-07-16 16:16:52 +0200

    Move classes to new logic based layer controller

---
+++ server/src/main/java/com/cafetamine/surveys/controller/SurveyController.java
@@ -0,0 +1,52 @@
+package com.cafetamine.surveys.controller;
+
+import com.cafetamine.surveys.model.Survey;
+import com.cafetamine.surveys.service.SurveyService;
+import org.springframework.web.bind.annotation.*;
+
+import com.cafetamine.surveys.service.UserService;
+
+
+@RestController
+@RequestMapping("surveys")
+public class SurveyController {
+
+    private SurveyService surveyService;
+    private UserService userService;
+
+    public SurveyController(SurveyService surveyService, UserService userService) {
+        this.surveyService = surveyService;
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public Iterable<Survey> getAll() {
+        return this.surveyService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Survey getById(@PathVariable Long id) {
+        return this.surveyService.getById(id);
+    }
+
+    @GetMapping(params = "user_id={id}")
+    public Iterable<Survey> getByAuthor(@RequestParam Long id) {
+        return this.surveyService.getByAuthor(this.userService.getById(id));
+    }
+
+    @PostMapping()
+    public Survey create(@RequestBody Survey survey) {
+        return this.surveyService.create(survey);
+    }
+
+    @PostMapping(value = "/{id}", params = "action=update")
+    public Survey update(@RequestBody Survey survey) {
+        return this.surveyService.update(survey);
+    }
+
+    @PostMapping(value = "/{id}", params = "action=delete")
+    public Survey delete(@PathVariable Long id) {
+        return this.surveyService.delete(id);
+    }
+
+}
---
+++ server/src/main/java/com/cafetamine/surveys/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.cafetamine.surveys.controller;
+
+import com.cafetamine.surveys.model.User;
+import com.cafetamine.surveys.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import com.cafetamine.surveys.service.SurveyService;
+
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    private UserService userService;
+    private SurveyService surveyService;
+
+    public UserController(UserService userService, SurveyService surveyService) {
+        this.userService = userService;
+        this.surveyService = surveyService;
+    }
+
+    @GetMapping()
+    public Iterable<User> getAll() {
+        return this.userService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return this.userService.getById(id);
+    }
+
+    @PostMapping()
+    public User signIn(@RequestBody User user) {
+        return this.userService.create(user);
+    }
+
+    // TODO when json w/o id is sent -> it's just m::create
+    // TODO  will prbl disperse with actual update implementation but keep eye
+    @PostMapping(value = "/{id}", params = "action=update")
+    public User update(@RequestBody User user) {
+        return this.userService.update(user);
+    }
+
+    @PostMapping(value = "/{id}", params = "action=delete")
+    public User archive(@PathVariable Long id) {
+        User user = this.userService.delete(id);
+        // TODO what witch surveys after he leaves?
+
+        return user;
+    }
+
+}
