df59dd7ff54d471d081d78df099ab9989cbecb52
abystry <arek.bystrowicz@gmail.com>
2018-07-16 16:38:20 +0200

    Add class attributes with neccessery presistence annotation createdAt, updatedAt

--- server/src/main/java/com/cafetamine/surveys/model/AuditModel.java
+++ server/src/main/java/com/cafetamine/surveys/model/AuditModel.java
@@ -1,14 +1,27 @@
 package com.cafetamine.surveys.model;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 import java.io.Serializable;
+import java.util.Date;
 
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AuditModel implements Serializable {
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private Date updatedAt;
+
 }
