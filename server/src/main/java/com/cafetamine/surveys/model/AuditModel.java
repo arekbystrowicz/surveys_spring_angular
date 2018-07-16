0590dd15059187d7b093e644869caefaa5924c99
abystry <arek.bystrowicz@gmail.com>
2018-07-16 16:35:22 +0200

    Add spring annotation for super class, and imlemented Serializable

--- server/src/main/java/com/cafetamine/surveys/model/AuditModel.java
+++ server/src/main/java/com/cafetamine/surveys/model/AuditModel.java
@@ -1,4 +1,14 @@
 package com.cafetamine.surveys.model;
 
-public abstract class AuditModel {
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import java.io.Serializable;
+
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AuditModel implements Serializable {
 }
