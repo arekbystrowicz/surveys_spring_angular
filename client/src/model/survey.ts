973bbca04816ac30a3e83fbef975945f1de69a65
abystry <arek.bystrowicz@gmail.com>
2018-07-29 11:47:16 +0200

    Add Surevy Object

---
+++ client/src/model/survey.ts
@@ -0,0 +1,21 @@
+import { User } from "./user";
+import { Category } from "./category";
+
+export class Survey {
+  id: number;
+  title: string;
+  description: string;
+  isPublished: boolean;
+  isAccessible: boolean;
+  author: User;
+  publishDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  author: User;
+  categories: Array<Category>;
+}
+
+
+
+
+
