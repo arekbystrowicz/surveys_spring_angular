c4dced1a2169ce9860b7f09e36264a22f880df7f
abystry <arek.bystrowicz@gmail.com>
2018-07-30 18:29:14 +0200

    Add httpClientModule as import

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { UserService } from "./service/user/user.service";
@@ -10,7 +11,8 @@ import { UserService } from "./service/user/user.service";
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
