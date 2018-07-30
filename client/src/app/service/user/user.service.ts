0dc004256d0e330ae44b384e40ae1902aee4651a
abystry <arek.bystrowicz@gmail.com>
2018-07-30 18:31:18 +0200

    Add signatures of neccessery methods

--- client/src/app/service/user/user.service.ts
+++ client/src/app/service/user/user.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
+import { User } from "../../model/user";
+
 @Injectable()
 export class UserService {
 
   constructor(private http: HttpClient) { }
+
+  public get(id: number) {
+
+  }
+
+  public getAll() {
+
+  }
+
+  public create(user: User) {
+
+  }
+
+  public update(user: User) {
+
+  }
+
+  public delete(user: User) {
+
+  }
+
+  public restore(user: User) {
+
+  }
 
 }
