a876c3508e535ce6a92a8b994bfe548e9ead38c4
abystry <arek.bystrowicz@gmail.com>
2018-07-31 15:45:43 +0200

    Add httpModule import and in constructor

--- client/src/app/service/category/category.service.ts
+++ client/src/app/service/category/category.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class CategoryService {
 
   private originUrl = '//localhost:8080/categories';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
 }
