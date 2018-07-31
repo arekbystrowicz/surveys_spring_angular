4351d469e8298f8641aadfe4973977b30cef9447
abystry <arek.bystrowicz@gmail.com>
2018-07-31 15:55:13 +0200

    Add httpModule as parameter in construtor

--- client/src/app/service/survey/survey.service.ts
+++ client/src/app/service/survey/survey.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+
+import { Survey } from "../../model/survey";
+
 
 @Injectable()
 export class SurveyService {
 
-  constructor() { }
+  private originUrl = '//localhost:8080/categories';
+
+  constructor(private http: HttpClient) { }
 
 }
