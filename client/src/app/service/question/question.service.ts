9aafc264ae7ccf404988f2bb7687cec98a24122b
abystry <arek.bystrowicz@gmail.com>
2018-07-31 16:02:42 +0200

    Add get method

--- client/src/app/service/question/question.service.ts
+++ client/src/app/service/question/question.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+
+import { Question } from "../../model/question";
 
 @Injectable()
 export class QuestionService {
@@ -7,5 +10,9 @@ export class QuestionService {
   private originUrl = '//localhost:8080/questions';
 
   constructor(private http: HttpClient) { }
+
+  public get(id: number): Observable<Question> {
+    return this.http.get<Question>(`${this.originUrl}/${id}`);
+  }
 
 }
