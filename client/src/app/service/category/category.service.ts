db79de9b18dc73e0a72abf3cf4f171ba0fc43443
abystry <arek.bystrowicz@gmail.com>
2018-07-31 15:47:10 +0200

    Add get method

--- client/src/app/service/category/category.service.ts
+++ client/src/app/service/category/category.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+
+import { Category } from "../../model/category";
+
 
 @Injectable()
 export class CategoryService {
@@ -7,5 +11,9 @@ export class CategoryService {
   private originUrl = '//localhost:8080/categories';
 
   constructor(private http: HttpClient) { }
+
+  public get(id: number): Observable<Category> {
+    return this.http.get<Category>(`${this.originUrl}/${id}`);
+  }
 
 }
