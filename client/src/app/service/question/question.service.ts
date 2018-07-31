0802e0afd8c1eb9866bb810c6994ddd6e4ccf7d4
abystry <arek.bystrowicz@gmail.com>
2018-07-31 16:00:39 +0200

    Add field with origin resource path

--- client/src/app/service/question/question.service.ts
+++ client/src/app/service/question/question.service.ts
@@ -4,6 +4,8 @@ import {HttpClient} from "@angular/common/http";
 @Injectable()
 export class QuestionService {
 
+  private originUrl = '//localhost:8080/questions';
+
   constructor(private http: HttpClient) { }
 
 }
