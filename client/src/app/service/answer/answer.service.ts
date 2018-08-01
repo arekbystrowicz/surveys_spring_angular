502c87a5249808189172e3ede94c381d64b8a340
abystry <arek.bystrowicz@gmail.com>
2018-08-01 14:15:00 +0200

    Add origin path as class field

--- client/src/app/service/answer/answer.service.ts
+++ client/src/app/service/answer/answer.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+
+import { Answer } from "../../model/answer";
 
 
 @Injectable()
 export class AnswerService {
 
+  private originUrl = '//localhost:8080/questions';
+
   constructor(private http: HttpClient) { }
-  
+
 }
