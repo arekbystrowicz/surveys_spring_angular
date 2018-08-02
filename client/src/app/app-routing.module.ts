40396c28316ec219dea9b208235aa1d2861b02a8
abystry <arek.bystrowicz@gmail.com>
2018-08-02 12:48:44 +0200

    Delete unnecessery CommonModule import, as well as imports and declarations for ngModule

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+
 })
 export class AppRoutingModule { }
