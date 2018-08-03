6b7a7b932c84089254243784ac095096cc6e0383
abystry <arek.bystrowicz@gmail.com>
2018-08-03 11:55:56 +0200

    Add route to users list

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { UserProfileComponent } from "./view/user/profile/user-profile.component";
+import { UserListComponent } from "./view/user/list/user-list.component";
 
 
 const routes: Routes = [
-// { path: 'users', component: UserProfileComponent },
+  { path: 'users', component: UserListComponent },
   { path: 'users/:id', component: UserProfileComponent }
 ];
 
