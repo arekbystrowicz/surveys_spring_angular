9abc5db207f5b1bbad331a7f72feed13ee77f099
abystry <arek.bystrowicz@gmail.com>
2018-08-03 11:01:08 +0200

    Add user list subscription

--- client/src/app/view/user/list/user-list.component.ts
+++ client/src/app/view/user/list/user-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
 import { UserService } from "../../../service/user/user.service";
+import { User } from "../../../model/user";
 
 
 @Component({
@@ -11,9 +12,18 @@ import { UserService } from "../../../service/user/user.service";
 })
 export class UserListComponent implements OnInit {
 
+  users: User[];
+
   constructor(private http: HttpClient, private userService: UserService) { }
 
+  private getAll(): void {
+    this.userService.getAll().subscribe(users => {
+      this.users = users;
+    });
+  }
+
   ngOnInit() {
+    this.getAll();
   }
 
 }
