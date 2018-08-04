3c44c242b3c2935f33d76a1c72bc746219ad0eec
abystry <arek.bystrowicz@gmail.com>
2018-08-04 08:39:56 +0200

    Add httpClient as dependency injection

--- client/src/app/view/survey/list/surevy-list.component.ts
+++ client/src/app/view/survey/list/surevy-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-surevy-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SurevyListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
