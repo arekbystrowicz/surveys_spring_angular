6dab7f2a23b8f5e706a4f3201900bc3e8957025e
abystry <arek.bystrowicz@gmail.com>
2018-08-04 09:30:20 +0200

    Add routing for SurveyPageComponent

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from "./view/user/profile/user-profile.component";
 import { UserListComponent } from "./view/user/list/user-list.component";
 import { SurveyListComponent } from "./view/survey/list/survey-list.component";
+import { SurveyPageComponent } from "./view/survey/page/survey-page.component";
 
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
   { path: 'users/:id', component: UserProfileComponent },
-  { path: 'surveys', component: SurveyListComponent }
+  { path: 'surveys', component: SurveyListComponent },
+  { path: 'surveys/:id', component: SurveyPageComponent }
 ];
 
 
