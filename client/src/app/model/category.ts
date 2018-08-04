e846edbcbf0b8e3138d2dbe59ac3cf9698335df0
abystry <arek.bystrowicz@gmail.com>
2018-08-04 14:13:11 +0200

    Implement deserializable service and deserialize method

--- client/src/app/model/category.ts
+++ client/src/app/model/category.ts
@@ -1,9 +1,22 @@
+import { Deserializable } from "./deserializable";
 import { User } from "./user";
 
-export class Category {
+
+export class Category implements Deserializable<Category> {
   id: number;
   tag: string;
   author: User;
   createdAt: Date;
   updatedAt: Date;
+
+  public deserialize(object: any): Category {
+    this.id = object.id;
+    this.tag = object.tag;
+    this.createdAt = object.createdAt;
+    this.updatedAt = object.updatedAt;
+    this.author = new User().deserialize(object.author);
+
+    return this;
+  }
+
 }
