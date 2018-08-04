79fb068a4bb18ca7d51f4df740db45324855f402
abystry <arek.bystrowicz@gmail.com>
2018-08-04 15:42:47 +0200

    Delete deserializable method and implementation of Deserializable

--- client/src/app/model/category.ts
+++ client/src/app/model/category.ts
@@ -1,22 +1,10 @@
-import { Deserializable } from "./deserializable";
 import { User } from "./user";
 
 
-export class Category implements Deserializable<Category> {
+export class Category {
   id: number;
   tag: string;
   author: User;
   createdAt: Date;
   updatedAt: Date;
-
-  public deserialize(object: any): Category {
-    this.id = object.id;
-    this.tag = object.tag;
-    this.createdAt = object.createdAt;
-    this.updatedAt = object.updatedAt;
-    this.author = new User().deserialize(object.author);
-
-    return this;
-  }
-
 }
