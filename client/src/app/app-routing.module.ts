f3158211c35d8c96fe84064b49973e711416d5f3
abystry <arek.bystrowicz@gmail.com>
2018-08-04 09:03:18 +0200

    Add path to surveys list in routing module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { UserProfileComponent } from "./view/user/profile/user-profile.component";
 import { UserListComponent } from "./view/user/list/user-list.component";
+import { SurveyListComponent } from "./view/survey/list/survey-list.component";
 
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
-  { path: 'users/:id', component: UserProfileComponent }
+  { path: 'users/:id', component: UserProfileComponent },
+  { path: 'surveys', component: SurveyListComponent }
 ];
 
 
