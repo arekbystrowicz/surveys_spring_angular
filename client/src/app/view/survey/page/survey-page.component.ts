e64dfa231f8ae14003e27f8ee071849d58bb1bf2
abystry <arek.bystrowicz@gmail.com>
2018-08-04 09:26:14 +0200

    Add dependency injection of SurveyService

--- client/src/app/view/survey/page/survey-page.component.ts
+++ client/src/app/view/survey/page/survey-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 
+import { SurveyService } from "../../../service/survey.service";
+
 
 @Component({
   selector: 'app-survey-page',
@@ -9,7 +11,7 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class SurveyPageComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private surveyService: SurveyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
