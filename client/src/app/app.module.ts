e2c638ce56138daa61988fef9269985584d5a8ca
abystry <arek.bystrowicz@gmail.com>
2018-08-04 09:22:31 +0200

    Generate new component SurveyPage

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AppRoutingModule } from './/app-routing.module';
 import { UserBadgeComponent } from './view/user/badge/user-badge.component';
 import { UserListComponent } from './view/user/list/user-list.component';
 import { SurveyListComponent } from './view/survey/list/survey-list.component';
+import { SurveyPageComponent } from './view/survey/page/survey-page.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { SurveyListComponent } from './view/survey/list/survey-list.component';
     UserProfileComponent,
     UserBadgeComponent,
     UserListComponent,
-    SurveyListComponent
+    SurveyListComponent,
+    SurveyPageComponent
   ],
   imports: [
     BrowserModule,
