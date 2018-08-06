e97d9535d45864d6f0d1f56cb61cf4eb855913bc
abystry <arek.bystrowicz@gmail.com>
2018-08-06 15:16:54 +0200

    Add forms module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 
@@ -32,7 +33,8 @@ import { EditorComponent } from './view/editor/editor.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     UserService,
