9c29b8212ef627561b48d88649de37304590cb9a
abystry <arek.bystrowicz@gmail.com>
2018-08-06 14:57:21 +0200

    Add EditorCompoenent to app-routiing module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { UserProfileComponent } from "./view/user/profile/user-profile.component
 import { UserListComponent } from "./view/user/list/user-list.component";
 import { SurveyListComponent } from "./view/survey/list/survey-list.component";
 import { SurveyPageComponent } from "./view/survey/page/survey-page.component";
+import { EditorComponent } from "./view/editor/editor.component";
 
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
   { path: 'users/:id', component: UserProfileComponent },
   { path: 'surveys', component: SurveyListComponent },
-  { path: 'surveys/:id', component: SurveyPageComponent }
+  { path: 'surveys/:id', component: SurveyPageComponent },
+  { path: 'editor?survey_id=:id', component: EditorComponent}
 ];
 
 
