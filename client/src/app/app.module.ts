f85b8020aacf052c2bedf20a296eb4e9586ffa2a
abystry <arek.bystrowicz@gmail.com>
2018-08-07 14:05:47 +0200

    Generate new compoenent QuestionForm

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { SurveyListComponent } from './view/survey/list/survey-list.component';
 import { SurveyPageComponent } from './view/survey/page/survey-page.component';
 import { EditorComponent } from './view/editor/editor.component';
 import { SurveyFormComponent } from './view/editor/survey-form/survey-form.component';
+import { QuestionFormComponent } from './view/editor/question-form/question-form.component';
 
 
 @NgModule({
@@ -30,7 +31,8 @@ import { SurveyFormComponent } from './view/editor/survey-form/survey-form.compo
     SurveyListComponent,
     SurveyPageComponent,
     EditorComponent,
-    SurveyFormComponent
+    SurveyFormComponent,
+    QuestionFormComponent
   ],
   imports: [
     BrowserModule,
