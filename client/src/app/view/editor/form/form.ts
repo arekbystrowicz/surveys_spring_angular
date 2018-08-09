2e03caa6cc60f980a9890b35e97cd63be344037a
abystry <arek.bystrowicz@gmail.com>
2018-08-09 14:23:13 +0200

    Add init method loading form attribiutes

--- client/src/app/view/editor/form/form.ts
+++ client/src/app/view/editor/form/form.ts
@@ -13,11 +13,35 @@ import { AnswerService } from "../../../service/answer/answer.service";
 export class SurveyForm {
 
   survey: Survey;
-  questions: Map<Question, Answer>;
+  questions: Map<Question, Answer[]> = new Map<Question, Answer[]>();
 
   constructor(private surveyService: SurveyService,
               private questionService: QuestionService,
               private answerService: AnswerService) {
+  }
+
+  public init(surveyId: number): void {
+    this.setSurvey(surveyId);
+    this.setQuestions(surveyId);
+  }
+
+  private setSurvey(surveyId: number): void {
+    this.surveyService.get(surveyId)
+      .subscribe(survey => this.survey = survey);
+  }
+
+  private setQuestions(surveyId: number): void {
+    this.questionService.getBySurveyId(surveyId)
+      .subscribe(questions => {
+        for (let question of questions) {
+          this.mapQuestionsWithAnswers(question);
+        }
+      });
+  }
+
+  private mapQuestionsWithAnswers(question: Question): void {
+    this.answerService.getAll(question.id)
+      .subscribe(answers => this.questions.set(question, answers));
   }
 
 }
