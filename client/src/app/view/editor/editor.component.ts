8ff86bdd07240952f34e72a1f861f4bdb00092f6
abystry <arek.bystrowicz@gmail.com>
2018-08-09 15:19:29 +0200

    Delete all Question and QuestionService references with Form

--- client/src/app/view/editor/editor.component.ts
+++ client/src/app/view/editor/editor.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 
-import { Question } from "../../model/question";
-
-import { QuestionService } from "../../service/question/question.service";
 import { SurveyForm } from "./form/form";
 
 
@@ -14,22 +11,12 @@ import { SurveyForm } from "./form/form";
 })
 export class EditorComponent implements OnInit {
 
-  questions: Question[];
-
   constructor(private form: SurveyForm,
-              private questionService: QuestionService,
               private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.getQuestions();
     this.form.init(+this.route.snapshot.paramMap.get('id'));
-  }
-
-  private getQuestions(): void {
-    const surveyId = +this.route.snapshot.paramMap.get('id');
-    this.questionService.getBySurveyId(surveyId)
-      .subscribe(questions => this.questions = questions);
   }
 
 }
