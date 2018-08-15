28e5c5c8f176ca6864280b2c48661d5782405c00
abystry <arek.bystrowicz@gmail.com>
2018-08-15 17:52:45 +0200

    Add getById method

--- server/src/main/java/com/cafetamine/surveys/category/CategoryService.java
+++ server/src/main/java/com/cafetamine/surveys/category/CategoryService.java
@@ -2,6 +2,8 @@ package com.cafetamine.surveys.category;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class CategoryService {
@@ -14,6 +16,15 @@ public class CategoryService {
 
     public Iterable<Category> getAll() {
         return this.categoryRepository.findAll();
+    }
+
+    public Category getById(Long id) {
+        Optional<Category> category = this.categoryRepository.findById(id);
+        if (!category.isPresent()) {
+            // TODO exception
+            throw new RuntimeException("404 category not found");
+        }
+        return category.get();
     }
 
 }
