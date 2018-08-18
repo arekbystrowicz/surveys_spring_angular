b9274b1db0bf9364f345362b0f60898807600dfc
abystry <arek.bystrowicz@gmail.com>
2018-08-18 18:23:09 +0200

    Add if block on addCategory method

--- client/src/app/view/editor/new-category-form/new-category-form.component.ts
+++ client/src/app/view/editor/new-category-form/new-category-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 import { SurveyForm } from "../form/form";
+import { Category } from "../../../model/category";
 
 @Component({
   selector: 'app-new-category-form',
@@ -10,10 +11,18 @@ import { SurveyForm } from "../form/form";
 export class NewCategoryFormComponent implements OnInit {
 
   @Input() form: SurveyForm;
+  category: Category = new Category();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public addCategory(): void {
+    if (this.category.tag) {
+      this.form.addCategory(this.category);
+      this.category = new Category();
+    }
   }
 
 }
