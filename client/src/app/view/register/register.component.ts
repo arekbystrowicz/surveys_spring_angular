ab4452769713b88fdc43c35de80b9ed5c7fed827
abystry <arek.bystrowicz@gmail.com>
2018-08-19 18:00:08 +0200

    Add user object as class field -> autoinitialized as empty

--- client/src/app/view/register/register.component.ts
+++ client/src/app/view/register/register.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from "../../model/user";
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
+  user: User = new User();
 
   constructor() { }
 
