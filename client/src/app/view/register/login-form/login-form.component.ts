0388887caaf356df31e4366a54f91d74d7c96737
abystry <arek.bystrowicz@gmail.com>
2018-08-19 20:33:04 +0200

    Add attrs user and loginIsValid passed via input from parent RegisterComponent

--- client/src/app/view/register/login-form/login-form.component.ts
+++ client/src/app/view/register/login-form/login-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { User } from "../../../model/user";
+
 
 @Component({
   selector: 'app-login-form',
@@ -6,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
+
+  @Input() user: User;
+  @Input() loginIsValid: boolean;
 
   constructor() { }
 
