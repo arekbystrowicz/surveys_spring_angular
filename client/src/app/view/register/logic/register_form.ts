de5c65f95eb0b6d181bfafe2a85eca11690c66be
abystry <arek.bystrowicz@gmail.com>
2018-08-20 11:32:31 +0200

    Add methods from RegisterCompoenent

--- client/src/app/view/register/logic/register_form.ts
+++ client/src/app/view/register/logic/register_form.ts
@@ -13,4 +13,14 @@ export class RegisterForm {
   constructor(private userService: UserService) {
   }
 
+  public register(): void {
+    this.userService.create(this.user)
+      .subscribe(response => this.user = response);
+  }
+
+  public isValid(): boolean {
+    return true;
+    // return this.passwordIsValid && this.loginIsValid && this.emailIsValid;
+  }
+
 }
