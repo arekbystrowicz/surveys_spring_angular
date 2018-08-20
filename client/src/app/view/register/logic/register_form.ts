c55b6d5926df5caa501e3f32d0f46d0141c6eb90
abystry <arek.bystrowicz@gmail.com>
2018-08-20 11:27:45 +0200

    Add user as class attribiute

--- client/src/app/view/register/logic/register_form.ts
+++ client/src/app/view/register/logic/register_form.ts
@@ -1,10 +1,14 @@
 import { Injectable } from "@angular/core";
 
+import { User } from "../../../model/user";
+
 import { UserService } from "../../../service/user/user.service";
 
 
 @Injectable()
 export class RegisterForm {
+
+  user: User = new User();
 
   constructor(private userService: UserService) {
   }
